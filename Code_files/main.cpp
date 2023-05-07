06faa20b2ab0abe70847bf3ce5ee1eb820b66b52
DenisBratus <bratus26d@gmail.com>
2023-05-07 22:33:04 +0300

    add generating script with python

--- Code_files/main.cpp
+++ Code_files/main.cpp
@@ -2,14 +2,22 @@
 #include <fstream>          //writing to file
 #include "SectionPoint.h"	// Header file with Point and Section classes
 #include <string>           //string class
-#include <cstdlib>
-using namespace std;
+#include <cstdlib>          //system
+#include "stdio.h"
 
+using namespace std;
+void StrCat(char *to,  char *from) {
+    char *p = to;
+    for(;*p != '\0'; ++p) {  }
+    *p = *from;
+    for(;*p !='\0';++p) *p = *from++;
+}
 int main() {
 
-
-
-
-
+    cout << "Enter <your_path_to_file> : " ;
+    string inputCom;
+    cin >> inputCom;
+    inputCom = "python " + inputCom;
+    system(inputCom.c_str() );
     return 0;
 }
