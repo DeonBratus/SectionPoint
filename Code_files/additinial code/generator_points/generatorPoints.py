b692fd10df24d9fc89cb006e45c88cd00f443849
Denis Bratus <107077899+DeonBratus@users.noreply.github.com>
2023-05-07 21:20:29 +0300

    Point generator

--- Code_files/additinial code/generator_points/generatorPoints.py
+++ Code_files/additinial code/generator_points/generatorPoints.py
@@ -1,14 +1,14 @@
 import csv
 import random
 
-# Генерируем 20 точек маршрута с координатами в диапазоне [-10, 10]
+# Generating coordinates values
 route_points = [(i, random.uniform(-10, 10)) for i in range(20)]
 
-# Сохраняем точки маршрута в CSV-файл
+# Save to scv file
 with open('route_points.csv', 'w', newline='') as csvfile:
     writer = csv.writer(csvfile)
-    writer.writerow(['x', 'y'])  # Записываем заголовок столбцов
+    writer.writerow(['x', 'y'])  # title
     for point in route_points:
         writer.writerow(point)
-
+# Print to console
 print("Массив точек маршрута сохранен в файл 'route_points.csv'")
