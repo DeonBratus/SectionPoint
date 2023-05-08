07f8af7dc550602fa0c34c346f112346c2cde84d
DenisBratus <bratus26d@gmail.com>
2023-05-08 20:23:15 +0300

    convert data and parsing

--- Code_files/main.cpp
+++ Code_files/main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>			//input, output
 #include <string>           //string class
 #include "SectionPoint.h"	// Header file with Point and Section classes
-
+using namespace std;
 int main() {
     int quantPoint = 21;                                        //Quantity of point and coordinates
-    std::string strArray[quantPoint + 1];                       //create array string more by one
+    string strArray[quantPoint + 1];                            //create array string more by one
     Point points[quantPoint];                                   // create points
 
-    std::string pathToFile; std::cin >> pathToFile;             //Take path to *.csv file
+    cout << "Enter absolute path to *.scv file for reading:";
+    string pathToFile; std::cin >> pathToFile;                  //Take path to *.csv file
     FileToLine(pathToFile,strArray);                            //reading *.csv file and convert data to line( string)
     parseLineToPoint(strArray, points,quantPoint); //parsing
 
